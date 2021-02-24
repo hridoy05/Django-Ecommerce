e9f98a0be790df199a6c6ac71c88863bdfe3f1a5
hridoy05 <hridoy.banik71@gmail.com>
2021-02-24 09:22:03 +0600

    on going site

---
+++ Ecommerce_Project/App_Shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from App_Shop.models import Product
+# Import views
+from django.views.generic import ListView, DetailView
+# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class Home(ListView):
+    model = Product
+    context_object_name = ''
+    template_name = 'App_Shop/home.html'
+
+
+class ProductDetail(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'App_Shop/product_detail.html'
